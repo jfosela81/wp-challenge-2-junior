5576591d553c2081c53908d892696296e1aeae39
Jorge Fosela <jfosela81@gmail.com>
2024-05-08 00:38:51 +0200

    Added styles just for pages with the shortcode embeded. Also added wp_mail for sending emails

--- shortcode.php
+++ shortcode.php
@@ -9,5 +9,19 @@ function jorge_render_contact_form( $att, $content ) {
 	ob_start();
 	require_once plugin_dir_path( __FILE__ ) . '/jorge-contact-form-template.php';
 	return ob_get_clean();
+}
 
+add_action( 'wp_enqueue_scripts', 'jorge_contact_form_styles' );
+function jorge_contact_form_styles() {
+
+	global $post;
+
+	if ( has_shortcode( $post->post_content, 'jorge-contact-form' ) ) {
+		wp_enqueue_style(
+			'jorge-contact-form-css',
+			plugin_dir_url( __FILE__ ) . 'style.css',
+			array(),
+			'1.0'
+		);
+	}
 }
